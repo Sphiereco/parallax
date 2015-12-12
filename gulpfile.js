096bd555b354c97f77542e553a96cfb5221d8ebf
Samuel Christopher <thesamchris@gmail.com>
2015-12-12 03:43:22 +0000

    Added jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require('gulp');
+var gulp      = require('gulp');
+var gulpJade  = require('gulp-jade');
+var jade      = require('jade');
+var outputDir = 'build';
+var buildType = process.env.buildtype || 'development';
 
-gulp.task('hello', function() {
-  console.log('hello gulp');
+gulp.task('html', function() {
+  var config = {
+    jade: jade
+  };
+
+  if (buildType === 'production') {
+    config.pretty = false;
+  } else {
+    config.pretty = true;
+  }
+
+  return gulp.src('./app/views/index.jade')
+        .pipe(gulpJade(config))
+        .pipe(gulp.dest(outputDir));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('./app/views/**/*.jade', ['html']);
+});
+
+gulp.task('default', ['html', 'watch']);
